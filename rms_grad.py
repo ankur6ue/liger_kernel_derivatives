fc5abc41ef84e82a07ef1c05c6710f6afeb9e88d
ankur6ue <ankur6ue@gmail.com>
2025-10-05 15:47:50 -0400

    Updated slides and code for lectures 1.1, 1.2, 1.3

--- rms_grad.py
+++ rms_grad.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 # compute derivative of RMS of a vector x and compare gradient calculated by autograd against manual calculation
+# Set the seed for reproducibility
+torch.manual_seed(42)
 n = 5
 x = torch.randn(1, n, requires_grad=True)
 x_squared = torch.pow(x, 2)
--- swiGLU_grad.py
+++ swiGLU_grad.py
@@ -1,27 +1,42 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 # compute derivative of swiGLU of a vector x and compare gradient calculated by autograd against manual calculation
 # See swiGLU section in https://arxiv.org/pdf/2410.10989 and swiglue{i}.jpg for derivation of manual derivative
-n = 10
-x = torch.randn(5, 1, requires_grad=True)
-W = torch.randn(10,5)
-V = torch.randn(10,5)
-b = torch.randn(10, 1)
-c = torch.randn(10, 1)
+# Set the seed for reproducibility
+torch.manual_seed(42)
+N = 10
+B = 3
+M = 5
+x = torch.randn(M, B, requires_grad=True)
+W = torch.randn(N,M, requires_grad=True)
+V = torch.randn(N,M)
+b = torch.randn(N, B, requires_grad=True )
+c = torch.randn(N, B)
 A1 = W @ x + b
 A2 = V @ x + c
-y = torch.mul(A1, A2)
-L = torch.sum(y)
 
 sigma_A1 = torch.divide(1, (1 + torch.exp(-A1)))
 A3 = A1*sigma_A1
 A4 = torch.mul(A3, A2)
+A4.retain_grad()
 L = torch.sum(A4)
 L.backward()
 print("dL_dx (autograd)")
 print(x.grad)
 
-# manual calculation
-dL_dx = torch.ones(1,10) @ (torch.diag(A3.squeeze())  @ V + torch.diag(A2.squeeze()) @  torch.diag(torch.mul(sigma_A1, (1 + A1 - torch.mul(A1, sigma_A1))).squeeze()) @ W)
+# Manual calculation
+# This expression is more explicit as it calculates the derivative of output of the hadamard product (A4 above) wrt
+# its inputs (A3, A2) explicitly (a N*N diagonal matrix) and then takes a dot product with dL_dA4 (all ones), but
+# will only work for a batch size of 1.. because of the torch.diag(A3.squeeze()), which doesn't translate to
+# > 1 batch sizes
+if B == 1:
+    dL_dx = V.T @ (torch.diag(A3.squeeze()) @ torch.ones(N, B)) + W.T @ (torch.diag(A2.squeeze()) @ torch.ones(N, B) * (sigma_A1 * (1 + A1 - A1 * sigma_A1)))
+
+# This should work for all batch sizes.. because for vectors of size A and V of dimensions N*B, A * V calculates the hadamard product of A and V
+# without using the diagonal matrix
+# @ means matrix multiplication, * means hadamard product
+dL_dx = V.T @ (A3 * torch.ones(N, B)) + W.T @ (A2 * (sigma_A1 * (1 + A1 - A1 * sigma_A1)) * torch.ones(N, B))
+
 print("dL_dx (manual)")
 print(dL_dx)
